c4ea37c4f4bc9c389a46d83e3fde35759425ef53
Lucas Johnson <lucasjjohnson12@gmail.com>
2025-02-27 22:45:36 -0800

    started with navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+    integrations: [icon()],
     vite: {
         ssr: {
             external: ["svgo"],
